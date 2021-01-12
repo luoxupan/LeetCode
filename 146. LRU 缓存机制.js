868d8361220a0bf9cfa8f3e53b8178391bf5f4be
luoxupan <luoxupan@didiglobal.com>
2021-01-12 19:37:59 +0800

    save

---
+++ 146. LRU 缓存机制.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+  this.map = new Map();
+  this.capacity = capacity;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+  let value = this.map.get(key);
+  if (value === undefined) {
+    return -1;
+  } else {
+    this.map.delete(key);
+    this.map.set(key, value);
+    return value;
+  }
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+  let _value = this.map.get(key);
+  if (_value === undefined) {
+    if (this.map.size >= this.capacity) {
+      this.map.delete(this.map.keys().next().value);
+    }
+  } else {
+    this.map.delete(key);
+  }
+  this.map.set(key, value)
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
