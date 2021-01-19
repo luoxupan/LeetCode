b8841675923ed45728c0706d2acd72d13cc39a0d
luoxupan <luoxupan@didiglobal.com>
2021-01-19 18:24:47 +0800

    save

---
+++ 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var exchange = function(nums) {
+  var l = 0;
+  var r = nums.length - 1;
+  while (l < r) {
+    while (l < r && nums[l] % 2 === 1) {
+      l++;
+    }
+    while (l < r && nums[r] % 2 === 0) {
+      r--;
+    }
+    if (l < r) {
+      var c = nums[l];
+      nums[l] = nums[r];
+      nums[r] = c;
+    }
+  }
+  return nums;
+};
