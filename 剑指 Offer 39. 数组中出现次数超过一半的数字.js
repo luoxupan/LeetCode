005dc18c240436ab1b8740685127cf3a32279c06
luoxupan <luoxupan@xiaomi.com>
2021-01-23 14:01:36 +0800

    save

---
+++ 剑指 Offer 39. 数组中出现次数超过一半的数字.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 解法：摩尔投票法
+ * 可以理解成混战极限一换一，不同的两者一旦遇见就同归于尽，最后活下来的值都是相同的，即要求的结果
+ * 时间O(n)，空间O(1)
+ */
+var majorityElement = function(nums) {
+  var count = 0;
+  var char = null;
+  for (var i = 0;i < nums.length; ++i) {
+    if (count === 0) {
+      char = nums[i];
+      count++;
+    } else {
+      count = char === nums[i] ? count + 1 : count - 1;
+    }
+  }
+  return char;
+};
